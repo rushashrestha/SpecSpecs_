9d8e43e69596167d01ce98affa6d3b9808ab8321
Rusha Shrestha <shrestharusha194@gmail.com>
2024-02-26 23:08:59 +0545

    fixed price

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CartItems from '../Components/CartItems/CartItems'
+import CartItems from '../Components/Assets/CartItems/CartItems'
 
 const Cart = () => {
   return (
