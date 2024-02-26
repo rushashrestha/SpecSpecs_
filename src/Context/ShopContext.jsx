fccc566afede1bd9aef5b61effffc69bb605c437
manjila <manjila2059@gmail.com>
2024-02-26 18:06:54 +0545

    context

---
+++ src/Context/ShopContext.jsx
@@ -0,0 +1,16 @@
+import React,{ createContext }  from "react";
+
+import allproducts from  '../Components/Assets/allproducts'
+export const ShopContext = createContext(null);
+
+const ShopContextProvider = (props) => {
+    const contextValue= {allproducts}
+
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider;
