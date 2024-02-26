d039bc72bee40d7fe85a4a62a70efdf7f36fca40
Mandira <mandeerashiwakoti@gmail.com>
2024-02-26 18:23:18 +0545

    commitment

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -13,10 +13,10 @@ const Navbar = () => {
            <p>Specspecs</p>
         </div>
         <ul className='nav-menu'>
-            <li onClick={()=>{setMenu('shop')}}>Shop{menu=='shop'?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu('mens')}}>Men{menu=='mens'?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu('womens')}}>Women{menu=='womens'?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu('kids')}}>Kids{menu=='kids'?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu('shop')}}>Shop{menu==='shop'?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu('mens')}}>Men{menu==='mens'?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu('womens')}}>Women{menu==='womens'?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu('kids')}}>Kids{menu==='kids'?<hr/>:<></>}</li>
 
         </ul>
         <div className='nav-login-cart'>
