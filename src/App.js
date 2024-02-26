e5cb86069450a387723fe88f7a2444dc9571041d
Rusha Shrestha <shrestharusha194@gmail.com>
2024-02-26 10:49:19 +0545

    Loader done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 
 import './App.css';
+
+import { useEffect, useState } from "react";
+
+import Loader from './Components/Loader/loading';
 import Navbar from './Components/Navbar/Navbar';
 
 function App() {
+
+  const [showLoader, setShowLoader] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowLoader(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  });
+
+  if (showLoader) {
+    return <Loader/>;
+  }
+
   return (
     <div >
     <Navbar/>
