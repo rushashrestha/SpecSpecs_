efdaff43ecba49217277a1b94264e717c106fd8b
manjila <manjila2059@gmail.com>
2024-02-28 16:30:29 +0545

    productdisplay

---
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './ProductDisplay.css'
+import star_icon from '../Assets/star_icon.png';
+
+const ProductDisplay = (props) => {
+    const {product}= props;
+  return (
+    <div className='productdisplay'>
+      <div className="productdisplay-left">
+        <div className="productdisplay-img-list">
+           <img src={product.image} alt="" />
+           <img src={product.image} alt="" />
+           <img src={product.image} alt="" />
+           <img src={product.image} alt="" />
+        </div>
+        <div className="productdisplay-img">
+            <img className='productdisplay-main-img' src={product.image} alt="" />
+        </div>
+      </div>
+      <div className="productdisplay-right">
+            <h1>{product.name}</h1>
+            <div className="productdisplay-right-star">
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                {/* <img src={star_dull_icon} alt="" /> */}
+                <p>(122)</p>
+            </div>
+            <div className="productdisplay-right-prices">
+                <div className="productdisplay-right-price-old">Rs.{product.old_price}</div>
+                <div className="productdisplay-right-price-new">Rs.{product.new_price}</div>
+            </div>
+            <div className="productdisplay-right-description">
+                Description
+            </div>
+            <div className="productdisplay-right-quantity">
+                <h1>Select quantity</h1>
+                
+            </div>
+            <button>ADD TO CART</button>
+            {/* <p className='productdisplay-right-category'><span>Category :</span></p> */}
+      </div>
+    </div>
+  )
+}
+
+export default ProductDisplay
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'
+import allproducts from '../Components/Assets/allproducts'
 
 const Product = () => {
   
   return (
     <div>
-      
+      <ProductDisplay product={allproducts}/>
     </div>
   )
 }
