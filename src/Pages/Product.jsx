184b38b9668e52ca3b9e458861bb019dc1e05bf8
Rusha Shrestha <shrestharusha194@gmail.com>
2024-02-28 16:26:02 +0545

    product done

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import ShopContext from '../Context/ShopContext'
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
+import allproducts from  '../Components/Assets/allproducts'
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+
 
 const Product = () => {
-  
+  const {allproducts} = useContext(ShopContext);
+  const {productID} = useParams();
+  const product = allproducts.find((e)=> e.id === Number(productID))
   return (
     <div>
-      
+      <Breadcrum product={product}/>
+      <ProductDisplay product={product}/>
     </div>
   )
 }
