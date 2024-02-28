67d3c714f83217a98544a66328e4b5b58dccb3b5
manjila <manjila2059@gmail.com>
2024-02-28 18:06:30 +0545

    nothing

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,12 +1,4 @@
 
-import React from 'react';
-import  { useContext } from 'react';
-import ShopContext from '../Context/ShopContext';
-import { useParams } from 'react-router-dom';
-import Breadcrum from '../Components/Breadcrums/Breadcrum';
-
-import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
-
 import React, { useContext } from "react";
 import ShopContext from "../Context/ShopContext";
 import { useParams } from "react-router-dom";
