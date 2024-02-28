9ae5550671ac4d7479c4282ff9dfde2a31f9f514
manjila <manjila2059@gmail.com>
2024-02-28 21:56:00 +0545

    cart

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,11 +1,9 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import './ProductDisplay.css'
 import star_icon from '../Assets/star_icon.png';
-import { ShopContext } from '../../Context/ShopContext';
 
 const ProductDisplay = (props) => {
     const {product}= props;
-    const {addToCart} = useContext(ShopContext);
   return (
     <div className='productdisplay'>
       <div className="productdisplay-left">
@@ -41,6 +39,7 @@ const ProductDisplay = (props) => {
                 
                 
             </div>
+            
             <button>ADD TO CART</button>
             {/* <p className='productdisplay-right-category'><span>Category :</span></p> */}
       </div>
@@ -49,3 +48,4 @@ const ProductDisplay = (props) => {
 }
 
 export default ProductDisplay
+
