d35ba2089b83e9448ab76fecd08cdbc047705255
Rusha Shrestha <shrestharusha194@gmail.com>
2024-02-28 17:44:58 +0545

    error fixed

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,24 +1,21 @@
-
-
-import React, { useContext } from 'react'
-import ShopContext from '../Context/ShopContext'
-import { useParams } from 'react-router-dom';
-import Breadcrum from '../Components/Breadcrums/Breadcrum';
-import allproducts from  '../Components/Assets/allproducts'
-import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
-
+import React, { useContext } from "react";
+import ShopContext from "../Context/ShopContext";
+import { useParams } from "react-router-dom";
+import Breadcrum from "../Components/Breadcrums/Breadcrum";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
+import allproducts from "../Components/Assets/allproducts";
 
 const Product = () => {
-  const {allproducts} = useContext(ShopContext);
-  const {productID} = useParams();
-  const product = allproducts.find((e)=> e.id === Number(productID))
+  
+  const { productID } = useParams();
+  const product = allproducts.find((e) => e.id === Number(productID));
   return (
     <div>
-      <ProductDisplay product={allproducts}/>
-      <Breadcrum product={product}/>
-      <ProductDisplay product={product}/>
+      <ProductDisplay product={allproducts} />
+      <Breadcrum product={product} />
+      <ProductDisplay product={product} />
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
