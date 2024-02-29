0749a2a4e6d619c2840520961225402012326515
manjila <manjila2059@gmail.com>
2024-02-29 12:21:17 +0545

    cart total

--- src/Components/Assets/CartItems/CartItems.jsx
+++ src/Components/Assets/CartItems/CartItems.jsx
@@ -3,7 +3,7 @@ import './CartItems.css';
 import { ShopContext } from "../../../Context/ShopContext";
 import remove_icon from '../removeicon.png';
 const CartItems = () => {
-    const {allproducts,cartItems,removeFromCart} = useContext(ShopContext)
+    const {getTotalCartAmount,allproducts,cartItems,removeFromCart} = useContext(ShopContext)
     return(
         <div className="cartitems">
             <div className="classitems-format-main">
@@ -20,19 +20,40 @@ const CartItems = () => {
                {
                 return (
                  <div >
-                <div className="cartitems-format">
+                <div className="cartitems-format cartitems-format-main">
                     <img src={e.image} alt="" className="carticon-product-icon"/>
                     <p>{e.name}</p>
                     <p>Rs.{e.new_price}</p>
                     <button className="cartitems-quantity">{cartItems[e.id]}</button>
                     <p>{e.new_price*cartItems[e.id]}</p>
-                    <img src={remove_icon} onClick= {()=>{removeFromCart(e.id)}}alt="" />
+                    <img className='cartitems-remove-icon'src={remove_icon} onClick= {()=>{removeFromCart(e.id)}}alt="" />
                 </div>
                 <hr />
             </div>
                 )
                } 
+               return null;
+
             })}
+            <div className="cartitems-down">
+                <div className="cartitems-total">
+                  <h1>cart Totals</h1>
+                  <div>
+                    <div className="cartitems-total-item">
+                        <p>Subtotal</p>
+                        <p>Rs{getTotalCartAmount()}</p>
+                    </div>
+                    <hr />
+                    <div className="cartitems-total-item">
+                        <h3>Total</h3>
+                        <h3>Rs{getTotalCartAmount()}</h3>
+                    </div>
+                  </div>
+                  <button>PROCEED TO CHECKOUT</button>
+                </div>
+               
+
+            </div>
 
         </div>
     )
