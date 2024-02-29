6347c2f077a139f0cd5daf17ed39ec487cad4770
dpkaaaaa <dpka060@gmail.com>
2024-02-29 08:20:58 +0545

    res

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,34 +1,86 @@
-import React, { useState } from 'react'
-import'./Navbar.css'
-import logo from '../Assets/logo.png'
-import cart from '../Assets/cart.png'
-import { Link, NavLink } from 'react-router-dom'
+import React, { useState, useRef } from "react";
+import "./Navbar.css";
+import logo from "../Assets/logo.png";
+import cart from "../Assets/cart.png";
+import { Link, NavLink } from "react-router-dom";
+// import nav_dropdown from "../Assets/dropdown_icon.png";
 const Navbar = () => {
-    
-     const[menu,setMenu]= useState('shop')
+  const [menu, setMenu] = useState("shop");
+  const menuRef = useRef();
+
+  const dropdown_toggle = (e) => {
+    menuRef.current.classlist.toggle("nav-menu-visible");
+    e.target.classlist.toggle("open");
+  };
 
   return (
-    <div className='navbar'>
-        <div className='nav-logo'>
-           <img width={50} src={logo} alt=''/>
-           <p>SPECSPECS</p>
-        </div>
-        <ul className='nav-menu'>
-           <li onClick={()=>{setMenu('shop')}}><Link  style={{textDecoration: 'none' }} to='/'>Shop</Link> {menu=='shop'?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu('vision')}}> <Link style={{textDecoration: 'none' }} to='/vision'>Vision</Link>{menu=='vision'?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu('sports')}}><Link style={{textDecoration: 'none' }} to='/sports'>Sports</Link>{menu=='sports'?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu('sunglass')}}><Link style={{textDecoration: 'none' }} to='/sunglass'>Sunglasses</Link>{menu=='sunglass'?<hr/>:<></>}</li>
-
-        </ul>
-        <div className='nav-login-cart'>
-            <Link to='/Login'><button>Login</button></Link>
-             <Link to='/cart'><img src={cart} alt=''></img> </Link>
-             <div className='nav-cart-count'>0</div>
-        </div>
-
-      
+    <div className="navbar">
+      <div className="nav-logo">
+        <img width={50} src={logo} alt="" />
+        <p>SPECSPECS</p>
+      </div>
+      {/* <div className="downdrop">
+        <img
+          className="nav-dropdown"
+          onClick={dropdown_toggle}
+          src={nav_dropdown}
+          alt=""
+        />
+      </div> */}
+      <ul ref={menuRef} className="nav-menu">
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/">
+            Shop
+          </Link>{" "}
+          {menu == "shop" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("vision");
+          }}
+        >
+          {" "}
+          <Link style={{ textDecoration: "none" }} to="/vision">
+            Vision
+          </Link>
+          {menu == "vision" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("sports");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/sports">
+            Sports
+          </Link>
+          {menu == "sports" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("sunglass");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/sunglass">
+            Sunglasses
+          </Link>
+          {menu == "sunglass" ? <hr /> : <></>}
+        </li>
+      </ul>
+      <div className="nav-login-cart">
+        <Link to="/Login">
+          <button>Login</button>
+        </Link>
+        <Link to="/cart">
+          <img src={cart} alt=""></img>{" "}
+        </Link>
+        <div className="nav-cart-count">0</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
