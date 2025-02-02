4f1e097dffe87d436dae090feff3f5637ef9e63a
Rusha Shrestha <shrestharusha194@gmail.com>
2025-02-02 17:31:41 +0545

    CSS updated

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,19 +1,32 @@
 import React from "react";
 import "./Item.css";
 import { Link } from "react-router-dom";
+import { FaPlus } from "react-icons/fa";
+
 const Item = (props) => {
   return (
     <div className="item">
       <div className="items">
         <div className="imgbox">
           <Link to={`/product/${props.id}`}>
-            <img  className="items-images" onClick={window.scrollTo(0, 0)} src={props.image}></img>
+            <img
+              className="items-images"
+              onClick={window.scrollTo(0, 0)}
+              src={props.image}
+              alt={props.name}
+            />
           </Link>
         </div>
-        <p>{props.name}</p>
-        <div className="item-prices">
-          <div className="item-price-new">Rs.{props.new_price}</div>
-          <div className="item-price-old">Rs.{props.old_price}</div>
+        <div className="item-details">
+          <div className="item-header">
+            <p className="item-name">{props.name}</p>
+            <button className="add-button"><FaPlus /></button>
+          </div>
+          <div className="item-prices">
+            <div className="item-price-new">रु{props.new_price}</div>
+            <div className="item-price-old">रु{props.old_price}</div>
+          </div>
+          <p className="item-category">{props.category}</p>
         </div>
       </div>
     </div>
