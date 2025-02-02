37ca3563ca1751846427ba85e3a029cc7696058c
Rusha Shrestha <shrestharusha194@gmail.com>
2025-02-02 20:39:02 +0545

    LatestCollection button

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,9 +3,17 @@ import "./Hero.css";
 import arrow_icon from "../Assets/arrow_icon.png";
 import hero_image from "../Assets/hero_image.png";
 import Typewriter from "typewriter-effect";
-import { Link } from "react-router-dom"; 
 
 const Hero = () => {
+  const scrollToPopularProducts = () => {
+    const popularProductsSection = document.getElementById("popular-products");
+    if (popularProductsSection) {
+      window.scrollTo({
+        top: popularProductsSection.offsetTop - 80, // Adjust for navbar height
+        behavior: "smooth",
+      });
+    }
+  };
   return (
     <div className="hero">
       <div className="hero-left">
@@ -30,10 +38,10 @@ const Hero = () => {
         
       
         <div className="hero-latest-btn">
-        <Link to="/popular" className="hero-latest-btn">
+        <a href="#popular-products" className="hero-latest-btn">
           <div>Latest Collection</div>
           <img src={arrow_icon} alt="Arrow Icon" />
-        </Link>
+        </a>
         </div>
       </div>
       <div className="hero-right">
