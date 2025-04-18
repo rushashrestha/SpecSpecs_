c04c36a08fa976c815a6612b43bf81b02f405713
Rusha Shrestha <shrestharusha194@gmail.com>
2025-04-18 18:02:57 +0545

    css updated

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -3,37 +3,47 @@ import "./CSS/ShopCategory.css";
 import { ShopContext } from "../Context/ShopContext";
 import Item from "../Components/Item/Item";
 
-const ShopCategory = (props) => {
-  console.log(useContext(ShopContext), "abc");
+
+const categoryTitles = {
+  Vision: "Frames That Define You",
+  Sports: "Gear Up Your Gaze",
+  Sunglasses: "Own the Shade",
+};
+
+const ShopCategory = ({ banner, category }) => {
   const { allproducts } = useContext(ShopContext);
+  const title = categoryTitles[category] || "Our Products";
+
   return (
     <div className="shop-category">
-      <div className="top">
-        
-          <img className="banner-img"  src={props.banner} alt="image" />
-       
-      </div>
-
+      {banner && (
+        <div className="shopcategory-banner">
+          <img
+            src={banner}
+            alt={`${category} banner`}
+            className="banner-img"
+          />
+        </div>
+      )}
 
       <div className="shopcategory-indexSort"></div>
-      <h1 className="our-product">Our Products</h1>
+
+
+      <h1 className="our-product">{title}</h1>
+
       <div className="shopcategory-product">
-        {allproducts.map((item, i) => {
-          if (props.category === item.category) {
-            return (
-              <Item
-                key={i}
-                id={item.id}
-                name={item.name}
-                image={item.image}
-                new_price={item.new_price}
-                old_price={item.old_price}
-              />
-            );
-          } else {
-            return null;
-          }
-        })}
+        {allproducts
+          .filter((item) => item.category === category)
+          .map((item) => (
+            <Item
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          ))}
       </div>
     </div>
   );
